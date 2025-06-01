dcd18f67bde6b042ad38bcfbf763a1374ebe790b
0rac130fD31phi <william.astley@algebraicwealth.com>
2025-06-01 07:47:01 -0400

    added the placeholder node

---
+++ src/haive/core/graph/node/placeholder_node.py
@@ -0,0 +1,10 @@
+from typing import Union
+
+from langgraph.types import Command
+from pydantic import BaseModel
+
+from haive.core.schema.state_schema import StateSchema
+
+
+def placeholder_node(state: Union[StateSchema, BaseModel]) -> Command[None]:
+    return Command(update={})
